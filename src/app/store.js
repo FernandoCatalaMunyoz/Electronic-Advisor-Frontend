fbbd45e19659848ad4c775a956c3b171ba146f60
Fernando <fernandocatalamunyoz@gmail.com>
2024-05-02 10:13:51 +0200

    confihguracion inicial e instalacion de dependencias que faltaban

---
+++ src/app/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import { thunk } from "redux-thunk";
+
+import storage from "redux-persist/lib/storage";
+import userSlice from "./slices/userSlice";
+import detailSlice from "./slices/detailSlice";
+
+const reducers = combineReducers({});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: "princess",
+      onError: function (error) {
+        // Handle the error.
+      },
+    }),
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(thunk),
+});
