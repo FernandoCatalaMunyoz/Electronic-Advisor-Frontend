fe2c4cb48347e597f8e16897408f6853a281ee66
Fernando <fernandocatalamunyoz@gmail.com>
2024-05-07 12:14:57 +0200

    feat: añado estilo a vista Users de superAdmin

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -2,16 +2,32 @@ import { useNavigate } from "react-router-dom";
 import "./User.css";
 import { useSelector } from "react-redux";
 import { userData } from "../../app/slices/userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { GetUsers } from "../../services/apicalls";
 
 export const Users = () => {
   const navigate = useNavigate();
   const rdxUser = useSelector(userData);
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     if (rdxUser?.credentials?.user?.roleName !== "super_admin") {
       navigate("/");
     }
   }, [rdxUser]);
-
-  return <div>Users</div>;
+  useEffect(() => {
+    if (users.length === 0) {
+      const bringUsers = async () => {
+        const fetchUsers = await GetUsers(rdxUser?.credentials?.token);
+        console.log(fetchUsers.data, "fetchUsers");
+        setUsers(fetchUsers.data);
+      };
+      bringUsers();
+    }
+  }, [users]);
+  return (
+    <div className="usersDesign">
+      <div className="usersListDesign">Lista</div>
+      <div className="createUserDesign">Crear</div>
+    </div>
+  );
 };
--- src/services/apicalls.js
+++ src/services/apicalls.js
@@ -111,3 +111,23 @@ export const GetEvents = async () => {
     return error;
   }
 };
+
+export const GetUsers = async (token) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    const response = await fetch(`${root}user`, options);
+    const data = await response.json();
+    if (!data.success) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
