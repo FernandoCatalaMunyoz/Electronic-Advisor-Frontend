6fb75c4b0f3b177a1fac230145ae8ded4506c4d9
Fernando <fernandocatalamunyoz@gmail.com>
2024-05-09 11:34:53 +0200

    feat: añadoi un poco de estiulo a vista detalle Evento

--- src/pages/EventDetail/EventDetail.jsx
+++ src/pages/EventDetail/EventDetail.jsx
@@ -7,34 +7,38 @@ export const EventDetail = () => {
   console.log(rdxDetail, "rdxDetail");
   return (
     <div className="eventDetailDesign">
-      <div className="eventDetailTitle">Evento</div>
-      <div className="eventDetailContent">
-        <div className="eventDetailName">{rdxDetail?.detail?.name}</div>
-        <div className="eventDetailDate">Event Date</div>
-        <div className="eventDetailClub">{rdxDetail?.detail?.club.name}</div>
-        <div className="eventDetailAddress">
-          {rdxDetail?.detail?.club.address}
+      <div className="eventInfo">
+        <div className="eventDetailTitle">Información del Evento</div>
+        <div className="eventDetailContent">
+          <div className="eventDetailName">{rdxDetail?.detail?.name}</div>
+          <div className="eventDetailClub">{rdxDetail?.detail?.club.name}</div>
+          <div className="eventDetailAddress">
+            {rdxDetail?.detail?.club.address}
+          </div>
+          <div className="eventDetailClubLink">
+            {rdxDetail?.detail?.club.link}
+          </div>
+          <div className="eventDetailDate">
+            {rdxDetail?.detail?.day +
+              "/" +
+              rdxDetail?.detail?.month +
+              "/" +
+              rdxDetail?.detail?.year}
+          </div>
         </div>
-        <div className="eventDetailClubLink">
-          {rdxDetail?.detail?.club.link}
-        </div>
-        <div className="eventDetailDate">
-          {rdxDetail?.detail?.day +
-            "/" +
-            rdxDetail?.detail?.month +
-            "/" +
-            rdxDetail?.detail?.year}
-        </div>
+      </div>
+      <div className="artistsInfo">
+        <div className="artistTitle">ARTISTAS</div>
         <div className="eventDetailArtists">
           {rdxDetail?.detail?.artistEvents.map((artist) => (
-            <div key={artist.id} className="eventDetailArtist">
+            <li key={artist.id} className="eventDetailArtist">
               {artist.artist.name}
-            </div>
+            </li>
           ))}
-          }
         </div>
-        {/* <div className="eventDetailImage">Event Image</div> */}
       </div>
+
+      {/* <div className="eventDetailImage">Event Image</div> */}
     </div>
   );
 };
